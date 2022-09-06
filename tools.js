e9727f38638ca2b01f5401b21eddb91715f26897
Anjali280 <mondalanjali0707@gmail.com>
2022-09-07 01:00:28 +0530

    Tools & Brushes feature

---
+++ tools.js
@@ -0,0 +1,319 @@
+
+  // var tools_array = [
+  //   {
+  //     tag: "Brush",
+  //     brand: "SEPHORA COLLECTION",
+  //     name: "Retractable Complexion Brush",
+  //     price: 1900,
+  //     img: "https://cdn13.nnnow.com/web-images/medium/styles/WISXKMTSCKV/1533194318322/1.jpg",
+  //   },
+  
+  //   {
+  //     tag: "Brush",
+  //     brand: "SEPHORA COLLECTION",
+  //     name: "Retractable Lip Brush",
+  //     price: 1800,
+  //     img: "https://cdn10.nnnow.com/web-images/medium/styles/E3ATWR0QI78/1533194318319/1.jpg",
+  //   },
+  
+  //   {
+  //     tag: "Brush",
+  //     brand: "MAKE UP FOR EVER",
+  //     name: "Instant Brush Cleanser",
+  //     price: 1800,
+  //     img: "https://cdn19.nnnow.com/web-images/medium/styles/X8JIWKM2HL1/1517466587226/1.jpg",
+  //   },
+  
+  //   {
+  //     tag: "Brush",
+  //     brand: "SMASHBOX",
+  //     name: "Lip Brush",
+  //     price: 2050,
+  //     img: "https://cdn05.nnnow.com/web-images/medium/styles/EM25HKPUWSF/1525433681246/1.jpg",
+  //   },
+  
+  //   {
+  //     tag: "Brush",
+  //     brand: "SMASHBOX",
+  //     name: "Buildable Cheek Brush",
+  //     price: 3600,
+  //     img: "https://cdn07.nnnow.com/web-images/medium/styles/B52J53900ZD/1525433681241/1.jpg",
+  //   },
+  
+  //   {
+  //     tag: "Brush",
+  //     brand: "BOBBI BROWN",
+  //     name: "Ultra Fine Eyeliner Brush",
+  //     price: 4200,
+  //     img: "https://cdn05.nnnow.com/web-images/medium/styles/RIGTLZO28BQ/1585223651841/1.jpg",
+  //   },
+  
+  //   {
+  //     tag: "Brush",
+  //     brand: "SHESEIDO",
+  //     name: "Daiya Fude Face Duo Brush",
+  //     price: 3600,
+  //     img: "https://cdn16.nnnow.com/web-images/medium/styles/S9HBP9JJUYI/1549870556596/1.jpg",
+  //   },
+  
+  //   {
+  //     tag: "Brush",
+  //     brand: "HUDA BEAUTY",
+  //     name: "Face Sculpt And Shade Dual Ended Contour And Bronze Complexion Brush",
+  //     price: 2300,
+  //     img: "https://cdn16.nnnow.com/web-images/medium/styles/JY31P159OKP/1600786072225/1.jpg",
+  //   },
+  
+  //   {
+  //     tag: "Brush",
+  //     brand: "STILLA",
+  //     name: "24 Double Ended Illuminating Powder Brush",
+  //     price: 3840,
+  //     img: "https://cdn13.nnnow.com/web-images/medium/styles/IN62GHTXOY8/1491393101968/1.jpg",
+  //   },
+  
+  //   {
+  //     tag: "Brush",
+  //     brand: "NUDESTIX",
+  //     name: "Blend + Buff Brush",
+  //     price: 2700,
+  //     img: "https://cdn08.nnnow.com/web-images/medium/styles/D0KROZI14LB/1586348072718/1.jpg",
+  //   },
+  
+  //   {
+  //     tag: "Tools",
+  //     brand: "SEPHORA COLLECTION",
+  //     name: "Face Konjac Sponge",
+  //     price: 980,
+  //     img: "https://cdn04.nnnow.com/web-images/medium/styles/LNOKEUIHEQC/1583391560346/1.jpg",
+  //   },
+  
+  //   {
+  //     tag: "Tools",
+  //     brand: "SEPHORA COLLECTION",
+  //     name: "Recycled Plastic Sharpner",
+  //     price: 400,
+  //     img: "https://cdn08.nnnow.com/web-images/medium/styles/CZLPWJPNK1M/1644913480935/1.jpg",
+  //   },
+  
+  //   {
+  //     tag: "Tools",
+  //     brand: "BENEFIT COSMETICS",
+  //     name: "Brow Grooming Tweezer & Brush",
+  //     price: 2070,
+  //     img: "https://cdn13.nnnow.com/web-images/medium/styles/MA46GGIYAG0/1588243022862/1.jpg",
+  //   },
+  
+  //   {
+  //     tag: "Tools",
+  //     brand: "STILA",
+  //     name: "Save the Day Eye & Lip Perfecter",
+  //     price: 1600,
+  //     img: "https://cdn07.nnnow.com/web-images/medium/styles/XKQ7A9PGLKD/1653470822275/1.jpg",
+  //   },
+  
+  //   {
+  //     tag: "Tools",
+  //     brand: "FOREO",
+  //     name: "Luna 3 for Combination Skin",
+  //     price: 18900,
+  //     img: "https://cdn10.nnnow.com/web-images/medium/styles/RU1UWVX0TTA/1636383743531/1.jpg",
+  //   },
+  
+  //   {
+  //     tag: "Tools",
+  //     brand: "MAC COSMETICS",
+  //     name: "Foundation Pump",
+  //     price: 1100,
+  //     img: "https://cdn15.nnnow.com/web-images/medium/styles/EV0341OXMDT/1600234110763/1.jpg",
+  //   },
+  
+  //   {
+  //     tag: "Tools",
+  //     brand: "Swati Cosmetics",
+  //     name: "False Lash Applicator",
+  //     price: 899,
+  //     img: "https://cdn06.nnnow.com/web-images/medium/styles/UWURL23LD87/1625733659837/1.jpg",
+  //   },
+  
+  //   {
+  //     tag: "Tools",
+  //     brand: "BEAUTYBLENDER",
+  //     name: "The Jewel Box Mystery Bling Box",
+  //     price: 2000,
+  //     img: "https://cdn03.nnnow.com/web-images/medium/styles/JX5IFLHLQ3J/1593779167620/1.jpg",
+  //   },
+  
+  //   {
+  //     tag: "Tools",
+  //     brand: "BEAUTYBLENDER",
+  //     name: "Blendercleanser® Liquid Charcoal Sponge & Brush Cleanser",
+  //     price: 2000,
+  //     img: "https://cdn11.nnnow.com/web-images/medium/styles/CNXQFZYEK3M/1634880939018/1.jpg",
+  //   },
+  
+  //   {
+  //     tag: "Tools",
+  //     brand: "FOREO",
+  //     name: "LUNA™ Mini 2 Facial Cleansing Device - All Skin Types",
+  //     price: 13490,
+  //     img: "https://cdn19.nnnow.com/web-images/medium/styles/BWUUNLVN3L6/1495780516926/1.jpg",
+  //   },
+  
+  //   {
+  //     tag: "Vegan",
+  //     brand: "SEPHORA COLLECTION",
+  //     name: "Classic Powder Brush #03",
+  //     price: 1500,
+  //     img: "https://cdn02.nnnow.com/web-images/medium/styles/Q44W3TYUVVE/1560852958775/1.jpg",
+  //   },
+  
+  //   {
+  //     tag: "Vegan",
+  //     brand: "SEPHORA COLLECTION",
+  //     name: "Classic Highlighter Brush #05",
+  //     price: 1200,
+  //     img: "https://cdn11.nnnow.com/web-images/medium/styles/RSNFZXONJ35/1560852958842/1.jpg",
+  //   },
+  
+  //   {
+  //     tag: "Vegan",
+  //     brand: "SEPHORA COLLECTION",
+  //     name: "Classic Smudge Brush #10",
+  //     price: 900,
+  //     img: "https://cdn13.nnnow.com/web-images/medium/styles/AW2FUPSFXQ7/1560852958851/1.jpg",
+  //   },
+  
+  //   {
+  //     tag: "Vegan",
+  //     brand: "SEPHORA COLLECTION",
+  //     name: "Classic Concealer Brush #02",
+  //     price: 1000,
+  //     img: "https://cdn12.nnnow.com/web-images/medium/styles/7VMQWKOB44R/1560852958779/1.jpg",
+  //   },
+  
+  //   {
+  //     tag: "Vegan",
+  //     brand: "SEPHORA COLLECTION",
+  //     name: "Classic Multitasker Brush #06",
+  //     price: 1500,
+  //     img: "https://cdn01.nnnow.com/web-images/medium/styles/6VSPH657G7L/1560852958771/1.jpg",
+  //   },
+  
+  //   {
+  //     tag: "Vegan",
+  //     brand: "SEPHORA COLLECTION",
+  //     name: "Classic Eyeshadow Brush #08",
+  //     price: 900,
+  //     img: "https://cdn02.nnnow.com/web-images/medium/styles/VTWEKSSQIHF/1560852958787/1.jpg",
+  //   },
+  
+  //   {
+  //     tag: "Vegan",
+  //     brand: "SEPHORA COLLECTION",
+  //     name: "Classic Foundation Brush #01",
+  //     price: 1200,
+  //     img: "https://cdn01.nnnow.com/web-images/medium/styles/JT4ECFC9Q96/1560852958783/1.jpg",
+  //   },
+  
+  //   {
+  //     tag: "Vegan",
+  //     brand: "SEPHORA COLLECTION",
+  //     name: "Classic Eyeliner Brush #11",
+  //     price: 900,
+  //     img: "https://cdn08.nnnow.com/web-images/medium/styles/62UZW0SPYH0/1560852958829/1.jpg",
+  //   },
+  
+  //   {
+  //     tag: "Vegan",
+  //     brand: "SEPHORA COLLECTION",
+  //     name: "Classic Kabuki Brush #07",
+  //     price: 2500,
+  //     img: "https://cdn02.nnnow.com/web-images/medium/styles/XOFHOPGA7UJ/1560852958766/1.jpg",
+  //   },
+  
+  //   {
+  //     tag: "Vegan",
+  //     brand: "SEPHORA COLLECTION",
+  //     name: "Classic Crease Brush #09",
+  //     price: 1200,
+  //     img: "https://cdn01.nnnow.com/web-images/medium/styles/BGVHWYX2T2H/1560852958832/1.jpg",
+  //   },
+  // ];
+   var toolBrushArray=JSON.parse(localStorage.getItem("tools_array")) || [];
+    display_products(toolBrushArray);
+
+  function display_products(t){
+    document.getElementById("tools").innerHTML = "";
+    t.map((elem) => {
+    var div = document.createElement("div");
+    var image = document.createElement("img");
+    image.setAttribute("src", elem.img);
+    image.setAttribute("alt", elem.name);
+  
+    var brand = document.createElement("h5");
+    brand.innerText = elem.brand;
+  
+    var name = document.createElement("p");
+    name.innerText = elem.name;
+  
+    var price = document.createElement("h5");
+    price.innerText = "Rs. " + elem.price;
+  
+    div.append(image, brand, name, price);
+    document.querySelector("#tools").append(div);
+  });
+}
+  //localStorage.setItem("tools_array" , JSON.stringify(tools_array));
+  function tags_filter()
+  {
+    select_tag = document.getElementById("tAg").value;
+     filtertag = toolBrushArray.filter(function(element)
+    {
+      if(select_tag === "tags")
+      {
+        return 1;
+      }
+        return element["tag"] === select_tag;
+  
+    })
+    display_products(filtertag)
+  }
+
+  function brands_filter()
+  {
+    select_brand = document.getElementById("brand").value;
+    filterbrand = toolBrushArray.filter(function(element)
+    {
+      if(select_brand === "brands")
+      {
+        return 1;
+      }
+      return element.brand === select_brand;
+    });
+    display_products(filterbrand)
+  }
+
+  function price_filter()
+  {
+    select_price=document.getElementById("price").value;
+    filterprice = toolBrushArray.filter(function(element)
+    {
+      if(select_price === "price"){
+        return 1;
+      }
+      else if(select_price === "0")
+      {
+        return element.price >= 0 && element.price < 2000;
+      }
+      else if(select_price === "2000")
+      {
+        return element.price >= 2000 && element.price < 5000;
+      }
+      else
+      {
+        return element.price >= 5000;
+      }
+    });
+    display_products(filterprice);
+  }
